178303e0c416fa188d3d790be15b2c70b2d6fc58
Jose Ortiz <dreamcpu@hotmail.com>
2021-01-25 20:18:08 -0500

    Your Lie in April Match 3 Game
    Match 3 Game made in Unity utilizing Your Lie in April Characters

---
+++ Match3/Match3/Assets/Scripts/FindMatches.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindMatches : MonoBehaviour
+{
+    private Board board;
+    public List<GameObject> matchedPieces = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+    }
+
+    public void FindAllMatches()
+    {
+        StartCoroutine(FindAllMatchesCo());
+    }
+
+    private IEnumerator FindAllMatchesCo()
+    {
+        yield return new WaitForSeconds(.2f);
+        for (int i = 0; i < board.width; i++)
+        {
+            for (int j = 0; j < board.height; j++)
+            {
+                GameObject currentDot = board.allDots[i, j];
+                if(currentDot != null)
+                {
+                    if(i > 0 && i < board.width -1)
+                    {
+                        GameObject leftDot = board.allDots[i - 1, j];
+                        GameObject rightDot = board.allDots[i + 1, j];
+                        if(leftDot != null && rightDot != null)
+                        {
+                            if(leftDot.tag == currentDot.tag && rightDot.tag == currentDot.tag)
+                            {
+                                if(!matchedPieces.Contains(leftDot))
+                                {
+                                    matchedPieces.Add(leftDot);
+                                }
+                                leftDot.GetComponent<Dot>().isMatched = true;
+                                if (!matchedPieces.Contains(rightDot))
+                                {
+                                    matchedPieces.Add(rightDot);
+                                }
+                                rightDot.GetComponent<Dot>().isMatched = true;
+                                if (!matchedPieces.Contains(currentDot))
+                                {
+                                    matchedPieces.Add(currentDot);
+                                }
+                                currentDot.GetComponent<Dot>().isMatched = true;
+                            }
+                        }
+                    }
+                    if (j > 0 && j < board.height - 1)
+                    {
+                        GameObject upDot = board.allDots[i, j +1];
+                        GameObject downDot = board.allDots[i, j - 1];
+                        if (upDot != null && downDot != null)
+                        {
+                            if (upDot.tag == currentDot.tag && downDot.tag == currentDot.tag)
+                            {
+                                if (!matchedPieces.Contains(upDot))
+                                {
+                                    matchedPieces.Add(upDot);
+                                }
+                                upDot.GetComponent<Dot>().isMatched = true;
+                                if (!matchedPieces.Contains(downDot))
+                                {
+                                    matchedPieces.Add(downDot);
+                                }
+                                downDot.GetComponent<Dot>().isMatched = true;
+                                if (!matchedPieces.Contains(currentDot))
+                                {
+                                    matchedPieces.Add(currentDot);
+                                }
+                                currentDot.GetComponent<Dot>().isMatched = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
